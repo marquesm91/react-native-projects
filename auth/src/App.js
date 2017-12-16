e7c15524b17497ab1f88dc0c4e226617b9d353da
marquesm91 <marquesm91@gmail.com>
2017-12-16 13:58:04 -0200

    Add App.js

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
+
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAfQWI33E4LWbZJggxt2R4RFA4g7UoL49k',
+      authDomain: 'auth-81acb.firebaseapp.com',
+      databaseURL: 'https://auth-81acb.firebaseio.com',
+      projectId: 'auth-81acb',
+      storageBucket: 'auth-81acb.appspot.com',
+      messagingSenderId: '649552011824'
+    });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <Text>An App!</Text>
+        <LoginForm />
       </View>
     );
   }
