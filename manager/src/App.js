10cd463345a2938c9180819964c6941eeb7dfe30
marquesm91 <marquesm91@gmail.com>
2017-12-17 11:38:29 -0200

    Add firebase and redux

--- manager/src/App.js
+++ manager/src/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyCQJjhG3Fz0oN3goy8gXtKX-uNEmBIa84A',
+      authDomain: 'manager-17f42.firebaseapp.com',
+      databaseURL: 'https://manager-17f42.firebaseio.com',
+      projectId: 'manager-17f42',
+      storageBucket: '',
+      messagingSenderId: '167982372113'
+    };
+
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore()}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello!
