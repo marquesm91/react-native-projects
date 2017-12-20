188afc63b19fea98605a3a5394bb10777ddc8dab
marquesm91 <marquesm91@gmail.com>
2017-12-20 10:48:55 -0200

    Finish Google Cloud Functions project

--- one-time-password/functions/index.js
+++ one-time-password/functions/index.js
@@ -1,6 +1,9 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./createUser');
+const requestOneTimePassword = require('./requestOneTimePassword');
+const verifyOneTimePassword = require('./verifyOneTimePassword');
+
 // Make sure you generate this JSON file on Firebase config project
 const serviceAccount = require ('./serviceAccount.json');
 
@@ -10,3 +13,5 @@ admin.initializeApp({
 });
 
 exports.createUser = functions.https.onRequest(createUser);
+exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword);
+exports.verifyOneTimePassword = functions.https.onRequest(verifyOneTimePassword);
---
+++ one-time-password/functions/requestOneTimePassword.js
@@ -0,0 +1,38 @@
+const admin = require('firebase-admin');
+// Make sure you get the accountSid and authToken from twilio website
+// and save on functions/twilio.js
+// Firebase and Twilio need Blaze upgrade. Be aware to get charges!
+const twilio = require('./twilio');
+
+module.exports = function (req, res) {
+  if (!req.body.phone) {
+    return res.status(422).send({ error: 'You must provide a phone number' });
+  }
+
+  const phone = String(req.body.phone).replace(/[^\d]/g, '');
+
+  admin.auth().getUser(phone)
+    .then(userRecord => {
+      const code = Math.floor((Math.random() * 8999 + 1000));
+
+      twilio.messages.create({
+        body: 'Your code is ' + code,
+        to: phone,
+        from: '+12526522350',
+      }, err => {
+        if (err) { return res.status(422).send(err); }
+
+        admin.database().ref('users/' + phone)
+          .update({ code: code, codeValid: true }, () => {
+            res.send({ success: true });
+          })
+
+      })
+    })
+    .catch(err => {
+      // development
+      res.status(422).send({ error: err });
+      // production
+      //res.status(422).send({ error: 'User not found.' });
+    });
+}
