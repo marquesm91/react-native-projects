40705b403ca04debff97f82455e8873ffc5f9b37
marquesm91 <marquesm91@gmail.com>
2017-12-20 22:43:57 -0200

    improve job app

--- jobs/App.js
+++ jobs/App.js
@@ -1,11 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Platform } from 'react-native';
+import { TabNavigator, StackNavigator } from 'react-navigation';
 
-export default class App extends React.Component {
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+import MapScreen from './screens/MapScreen';
+import DeckScreen from './screens/DeckScreen';
+import SettingsScreen from './screens/SettingsScreen';
+import ReviewScreen from './screens/ReviewScreen';
+
+class App extends Component {
   render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen },
+      main: {
+        screen: TabNavigator({
+          map: { screen: MapScreen },
+          deck: { screen: DeckScreen },
+          review: {
+            screen: StackNavigator({
+              review: { screen: ReviewScreen },
+              settings: { screen: SettingsScreen }
+            })
+          }
+        }, {
+          tabBarPosition: 'bottom',
+          animationEnabled: true,
+          tabBarOptions: {
+            activeTintColor: '#e91e63',
+          },
+        })
+      }
+    }, {
+      tabBarPosition: 'bottom',
+      animationEnabled: true,
+      tabBarOptions: {
+        activeTintColor: '#e91e63',
+      },
+    });
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <MainNavigator />
       </View>
     );
   }
@@ -15,7 +52,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Platform.OS === 'android' ? 24 : 0
   },
 });
+
+export default App;
