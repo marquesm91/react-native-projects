6400a6a13ca19e763518f411f0dd1e01cd7755aa
marquesm91 <marquesm91@gmail.com>
2017-12-28 11:33:37 -0200

    Add more components and update old ones to get ajusted

--- awesome-places/src/components/PlaceInput/PlaceInput.js
+++ awesome-places/src/components/PlaceInput/PlaceInput.js
@@ -1,53 +1,13 @@
-import React, { Component } from 'react';
-import { StyleSheet, View, TextInput, Button } from 'react-native';
+import React from 'react';
+//import { StyleSheet, View, TextInput, Button } from 'react-native';
+import DefaultInput from '../UI/DefaultInput/DefaultInput';
 
-class PlaceInput extends Component {
-  state = { placeName: '' };
+const placeInput = props => (
+  <DefaultInput
+    placeholder="Place name"
+    value={props.placeName}
+    onChangeText={props.onChangeText}
+  />
+);
 
-  placeNameChangedHandler = value => {
-    this.setState({ placeName: value })
-  }
-
-  placeSubmitHandler = () => {
-    if (this.state.placeName.trim() === '') {
-      return;
-    }
-
-    this.props.onPlaceAdded(this.state.placeName);
-  }
-
-  render() {
-    return (
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.placeInput}
-          placeholder={'An awesome place'}
-          value={this.state.placeName}
-          onChangeText={this.placeNameChangedHandler}
-        />
-        <Button
-          style={styles.placeButton}
-          title={'Add'}
-          onPress={this.placeSubmitHandler}
-        />
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  inputContainer: {
-    width: "100%",
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  placeInput: {
-    width: '70%',
-  },
-  placeButton: {
-    width: '30%'
-  }
-});
-
-export default PlaceInput;
+export default placeInput;
