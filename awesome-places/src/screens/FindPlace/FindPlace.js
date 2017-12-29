641c19eb00c90763dd2a01ae63aed7f079db47ae
marquesm91 <marquesm91@gmail.com>
2017-12-29 11:07:01 -0200

    give a nice animation to 'Find Places' button

--- awesome-places/src/screens/FindPlace/FindPlace.js
+++ awesome-places/src/screens/FindPlace/FindPlace.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Animated
+} from 'react-native';
 import { connect } from 'react-redux';
 
 import PlaceList from '../../components/PlaceList/PlaceList';
@@ -9,7 +15,10 @@ class FindPlace extends Component {
     navBarButtonColor: 'orange'
   }
 
-  state = { placesLoaded: false }
+  state = {
+    placesLoaded: false,
+    removeAnim: new Animated.Value(1)
+  }
 
   constructor(props) {
     super(props);
@@ -29,9 +38,11 @@ class FindPlace extends Component {
   }
 
   placesSearchHandler = () => {
-    this.setState({
-      placesLoaded: true
-    });
+    Animated.timing(this.state.removeAnim, {
+      toValue: 0,
+      duration: 500, // milliseconds
+      useNativeDriver: true
+    }).start();
   }
 
   itemSelectedHandler = key => {
@@ -46,11 +57,25 @@ class FindPlace extends Component {
 
   render() {
     let content = (
-      <TouchableOpacity onPress={this.placesSearchHandler}>
-        <View style={styles.searchButton}>
-          <Text style={styles.searchButtonText}>Find Places</Text>
-        </View>
-      </TouchableOpacity>
+      <Animated.View
+        style={{
+          opacity: this.state.removeAnim,
+          transform: [
+            {
+              scale: this.state.removeAnim.interpolate({
+                inputRange: [0, 1],
+                outputRange: [12, 1]
+              })
+            }
+          ]
+        }}
+      >
+        <TouchableOpacity onPress={this.placesSearchHandler}>
+          <View style={styles.searchButton}>
+            <Text style={styles.searchButtonText}>Find Places</Text>
+          </View>
+        </TouchableOpacity>
+      </Animated.View>
     );
 
     if (this.state.placesLoaded) {
