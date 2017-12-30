0889e78ade2bc2e567d221589244b1bc519f78bc
marquesm91 <marquesm91@gmail.com>
2017-12-30 15:51:20 -0200

    Place Detail show the image's location

--- awesome-places/src/screens/PlaceDetail/PlaceDetail.js
+++ awesome-places/src/screens/PlaceDetail/PlaceDetail.js
@@ -9,6 +9,7 @@ import {
   Dimensions
 } from 'react-native';
 import { connect } from 'react-redux';
+import MapView from 'react-native-maps';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 import { deletePlace } from '../../store/actions';
@@ -39,6 +40,13 @@ class PlaceDetail extends Component {
   }
 
   render() {
+    const initialRegion = {
+      latitude: this.props.selectedPlace.location.latitude,
+      longitude: this.props.selectedPlace.location.longitude,
+      latitudeDelta: 0.0122,
+      longitudeDelta: Dimensions.get("window").width / Dimensions.get("window").height * 0.0122
+    }
+
     return (
       <View
         style={[
@@ -48,13 +56,35 @@ class PlaceDetail extends Component {
             : styles.landscapeContainer
         ]}
       >
-        <View style={{ flex: 1 }}>
-          <Image
-            source={this.props.selectedPlace.image}
-            style={styles.placeImage}
-          />
+        <View
+          style={[
+            styles.imageAndMapContainer,
+            this.state.viewMode === "portrait"
+              ? styles.portraitContainer
+              : styles.landscapeContainer
+          ]}
+        >
+          <View style={styles.subContainer}>
+            <Image
+              source={this.props.selectedPlace.image}
+              style={styles.placeImage}
+            />
+          </View>
+          <View style={styles.subContainer}>
+            <MapView
+              initialRegion={{
+                latitude: this.props.selectedPlace.location.latitude,
+                longitude: this.props.selectedPlace.location.longitude,
+                latitudeDelta: 0.0122,
+                longitudeDelta: Dimensions.get("window").width / Dimensions.get("window").height * 0.0122
+              }}
+              style={styles.mapContainer}
+            >
+              <MapView.Marker coordinate={this.props.selectedPlace.location} />
+            </MapView>
+          </View>
         </View>
-        <View style={{ flex: 1 }}>
+        <View style={styles.textAndButtonsContainer}>
           <View>
             <Text style={styles.placeName}>
                 {this.props.selectedPlace.name}
@@ -79,11 +109,31 @@ class PlaceDetail extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    margin: 22,
     flex: 1
   },
+  imageAndMapContainer: {
+    flex: 2
+  },
+  textAndButtonsContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  subContainer: {
+    flex: 1
+  },
+  mapContainer: {
+    // same as ...StyleSheet.absoluteFillObject
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0
+  },
   placeImage: {
-    width: "100%",
-    height: 200
+    width: '100%',
+    height: '100%'
   },
   placeName: {
     fontWeight: 'bold',
