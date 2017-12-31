343a731b4106938f91800e89fbbc534e10e12363
marquesm91 <marquesm91@gmail.com>
2017-12-31 11:57:54 -0200

    update function to handle authentication tokens

--- awesome-places/functions/index.js
+++ awesome-places/functions/index.js
@@ -1,4 +1,5 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const cors = require('cors')({ origin: true });
 const fs = require('fs');
 const UUID = require('uuid-v4');
@@ -10,50 +11,83 @@ const UUID = require('uuid-v4');
               generate a private key. Download and save it next to this index.js
 ================================================================================
 
+  // inside gcconfig.js
+
   const gcconfig = {
     projectId: 'your-project-id',
     keyFilename: 'your-private-key-json-file-inside-index.json'
   }
 
+  module.exports = gcconfig;
+
 */
 const gcconfig = require('./gcconfig');
 const gcs = require('@google-cloud/storage')(gcconfig);
 
+admin.initializeApp({
+  credential: admin.credential.cert(require('./awesome-places.json'))
+});
+
 exports.storeImage = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
-    const body = JSON.parse(request.body);
+    if (
+      !request.headers.authorization ||
+      !request.headers.authorization.startsWith('Bearer ')
+    ) {
+      console.log('No token present!');
+      response.status(403).json({ error: 'Unauthorized' });
+      return;
+    }
 
-    fs.writeFileSync('/tmp/uploaded-image.jpg', body.image, 'base64', err => {
-      console.log(err);
-      return reponse.status(500).json({ error: err });
-    });
+    let idToken;
+    idToken = request.headers.authorization.split('Bearer ')[1];
+    admin
+      .auth()
+      .verifyIdToken(idToken)
+      .then(decodedToken => {
+        const body = JSON.parse(request.body);
 
-    const bucket = gcs.bucket('awesome-places-1514600820487.appspot.com');
-    const uuid = UUID();
+        fs.writeFileSync(
+          '/tmp/uploaded-image.jpg',
+          body.image,
+          'base64',
+          err => {
+            console.log(err);
+            return reponse.status(500).json({ error: err });
+          }
+        );
 
-    bucket.upload('/tmp/uploaded-image.jpg', {
-      uploadType: 'media',
-      destination: '/places/' + uuid + '.jpg',
-      metadata: {
-        metadata: {
-          contentType: 'image/jpeg',
-          firebaseStorageDownloadTokens: uuid
-        }
-      }
-    }, (err, file) => {
-      if (!err) {
-        response.status(201).json({
-          imageUrl: 'https://firebasestorage.googleapis.com/v0/b/' +
-            bucket.name +
-            '/o/' +
-            encodeURIComponent(file.name) +
-            '?alt=media&token=' +
-            uuid
+        const bucket = gcs.bucket('awesome-places-1514600820487.appspot.com');
+        const uuid = UUID();
+
+        bucket.upload('/tmp/uploaded-image.jpg', {
+          uploadType: 'media',
+          destination: '/places/' + uuid + '.jpg',
+          metadata: {
+            metadata: {
+              contentType: 'image/jpeg',
+              firebaseStorageDownloadTokens: uuid
+            }
+          }
+        }, (err, file) => {
+          if (!err) {
+            response.status(201).json({
+              imageUrl: 'https://firebasestorage.googleapis.com/v0/b/' +
+                bucket.name +
+                '/o/' +
+                encodeURIComponent(file.name) +
+                '?alt=media&token=' +
+                uuid
+            });
+          } else {
+            console.log(err);
+            response.status(500).json({ error: err });
+          }
         });
-      } else {
-        console.log(err);
-        response.status(500).json({ error: err });
-      }
-    });
+      })
+      .catch(err => {
+        console.log('Token is invalid!');
+        response.status(403).json({ error: 'Unauthorized' });
+      });
   });
 });
